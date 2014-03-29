d43f3ad7d11568729dda1266234e7b1355e1b10a
Jamie Shaw <jamie.e.shaw@gmail.com>
2014-03-29 19:24:44 -0400

    More refactoring

---
+++ src/com/weatherapp/model/DailyForecast.java
@@ -0,0 +1,39 @@
+package com.weatherapp.model;
+
+public interface DailyForecast {
+
+	public abstract String getLongDay();
+
+	public abstract void setLongDay(String longDay);
+
+	public abstract String getShortDay();
+
+	public abstract void setShortDay(String shortDay);
+
+	public abstract String getShortPrediction();
+
+	public abstract void setShortPrediction(String shortPrediction);
+
+	public abstract String getLongPrediction();
+
+	public abstract void setLongPrediction(String longPrediction);
+
+	public abstract String getTemperature();
+
+	public abstract void setTemperature(String temperature);
+
+	public abstract String getImageURL();
+
+	public abstract String getImageName();
+
+	public abstract void setImageName(String imageName);
+
+	public abstract void setImageURL(String imageURL);
+
+	public abstract boolean isNight();
+
+	public abstract void setNight(boolean isNight);
+
+	public abstract String toString();
+
+}
---
+++ src/com/weatherapp/model/DefaultHourlyForecast.java
@@ -0,0 +1,102 @@
+package com.weatherapp.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import com.weatherapp.service.ServiceFactory;
+
+public class DefaultHourlyForecast implements HourlyForecast, Serializable {
+
+	protected static final long serialVersionUID = 1L;
+
+	protected String chancePrecip;
+	protected long dateTime;
+	protected String desc;
+	protected String dewPoint;
+	protected String feelsLike;
+	protected String feelsLikeLabel;
+	protected String humidity;
+	protected String icon;
+	protected String skyCover;
+	protected String temperature;
+	protected String windDir;
+	protected String windSpeed;
+
+	@Override
+	public String getWindChill() {
+		return feelsLike;
+	}
+
+	@Override
+	public String getTemp() {
+		return temperature;
+	}
+
+	@Override
+	public String getImageName() {
+		return icon;
+	}
+
+	@Override
+	public String getImageURL() {
+		return ServiceFactory.getImageService().getIconUrl(icon);
+	}
+
+	@Override
+	public String getLongPrediction() {
+		return desc;
+	}
+
+	public void setChancePrecip(String chancePrecip) {
+		this.chancePrecip = chancePrecip;
+	}
+
+	public Date getDateTime() {
+		return new Date(Long.valueOf(dateTime));
+	}
+
+	public void setDateTime(String dateTime) {
+		long dateTimeSeconds = Long.parseLong(dateTime);
+		this.dateTime = dateTimeSeconds * 1000L;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public void setDewPoint(String dewPoint) {
+		this.dewPoint = dewPoint;
+	}
+
+	public void setFeelsLike(String feelsLike) {
+		this.feelsLike = feelsLike;
+	}
+
+	public void setFeelsLikeLabel(String feelsLikeLabel) {
+		this.feelsLikeLabel = feelsLikeLabel;
+	}
+
+	public void setHumidity(String humidity) {
+		this.humidity = humidity;
+	}
+
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
+
+	public void setSkyCover(String skyCover) {
+		this.skyCover = skyCover;
+	}
+
+	public void setTemperature(String temperature) {
+		this.temperature = temperature;
+	}
+
+	public void setWindDir(String windDir) {
+		this.windDir = windDir;
+	}
+
+	public void setWindSpeed(String windSpeed) {
+		this.windSpeed = windSpeed;
+	}
+}
